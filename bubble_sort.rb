b58a68dfadade111b2e6202c1c19b4f05910359a
gasb150 <gasb_150@hotmail.com>
2020-11-24 16:19:59 -0500

    change pass name to i

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,16 +1,16 @@
 def bubble_sort(array)
   n = array.length
   if n<=1 then  return array end
-  pass = 0
-  while pass < n
+  i = 0
+  while i < n
      step = 1
-    while step < (n - pass)
+    while step < (n - i)
       if array[step - 1] > array[step]
-        array[step - 1] ,array[step] = array[step] ,array[step - 1]
+        array[step - 1], array[step] = array[step], array[step - 1]
       end
       step += 1
     end
-    pass += 1
+    i += 1
   end
   array
 end
@@ -19,25 +19,19 @@ p bubble_sort([4, 3, 78, 2, 0, 2])
 
 def bubble_sort_by(array)
   n = array.length
-  if n <=1 then return array end
-
-  pass = 0
-  is_sorted = false
-  while pass < n
-    is_sorted = true
+  if n <= 1 then return array end
+  i = 0
+  while i < n    
     step = 1
-    while step < (n - pass)
+    while step < (n - i)
       if yield(array[step - 1], array[step]).positive?
-        array[step - 1], array[step] = array[step], array[step - 1]
-        is_sorted = false
+        array[step - 1], array[step] = array[step], array[step - 1]        
       end
       step += 1
-    end
-    if is_sorted then return array end
-
-    pass += 1
+    end    
+    i += 1
   end
   array
 end
 
-p bubble_sort_by(['hi', 'hello', 'hey']) { |left, right| left.length - right.length }
+p bubble_sort_by(%w('hi hello hey')) { |left, right| left.length - right.length }
