924e43d438f36aed0981b3fed1f81a002d27a954
gasb150 <gasb_150@hotmail.com>
2020-11-24 16:11:31 -0500

    fix linters

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,49 +1,43 @@
 def bubble_sort(array)
   n = array.length
-  if n<=1
-    return array
-  end
+  if n<=1 then  return array end
   pass = 0
-   while pass<n do
+  while pass < n
      step = 1
-    while step<(n-pass) do
-      if array[step-1]>array[step]
-        array[step-1], array[step] = array[step], array[step-1]
+    while step < (n - pass)
+      if array[step - 1] > array[step]
+        array[step - 1] ,array[step] = array[step] ,array[step - 1]
       end
-      step +=1
+      step += 1
     end
-    pass +=1
+    pass += 1
   end
   array
 end
 
-p bubble_sort([4,3,78,2,0,2])
-
+p bubble_sort([4, 3, 78, 2, 0, 2])
 
 def bubble_sort_by(array)
   n = array.length
-  if n<=1
-    return array
-  end
+  if n <=1 then return array end
 
   pass = 0
-  isSorted = false
-  while pass<n do
-    isSorted = true
+  is_sorted = false
+  while pass < n
+    is_sorted = true
     step = 1
-    while step<(n-pass) do
-      if yield(array[step-1] ,array[step]) > 0 
-        array[step-1], array[step] = array[step], array[step-1]
-        isSorted = false
+    while step < (n - pass)
+      if yield(array[step - 1], array[step]).positive?
+        array[step - 1], array[step] = array[step], array[step - 1]
+        is_sorted = false
       end
-      step +=1
+      step += 1
     end
-    if isSorted
-      return array
-    end
-    pass +=1
+    if is_sorted then return array end
+
+    pass += 1
   end
   array
 end
 
-p bubble_sort_by(["hi","hello","hey"]){ |left,right| left.length - right.length}
+p bubble_sort_by(['hi', 'hello', 'hey']) { |left, right| left.length - right.length }
