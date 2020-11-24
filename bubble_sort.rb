5bf26fe4fcc0a3232a807d2a5dd6c82a5966ca53
gasb150 <gasb_150@hotmail.com>
2020-11-24 16:50:57 -0500

    fix linters

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -5,20 +5,19 @@ def bubble_sort(array)
   while pass < n
     step = 1
     while step < (n - pass)
-     array[step - 1], array[step] = array[step], array[step - 1] if array[step - 1] > array[step]
-     step += 1
+      array[step - 1], array[step] = array[step], array[step - 1] if array[step - 1] > array[step]
+      step += 1
     end
     pass += 1
-   end
-   array
+  end
+  array
 end
 
 p bubble_sort([4, 3, 78, 2, 0, 2])
 
 def bubble_sort_by(array)
   n = array.length
-  return array if n <= 1
-  
+  return array if n <= 1  
   pass = 0
   while pass < n
     step = 1
