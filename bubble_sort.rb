442c1b76ce5876a25d096d316cb453c8bf696927
gasb150 <gasb_150@hotmail.com>
2020-11-24 16:42:15 -0500

    fix linters

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,16 +1,14 @@
 def bubble_sort(array)
   n = array.length
-  return array if n<=1
+  return array if n <= 1
   pass = 0
   while pass < n
-     step = 1
-    while step < (n - pass)
-      if array[step - 1] > array[step]
-        array[step - 1] ,array[step] = array[step] ,array[step - 1]
-      end
-      step += 1
-    end
-    pass += 1
+   step = 1
+   while step < (n - pass)
+     array[step - 1], array[step] = array[step], array[step - 1] if array[step - 1] > array[step]
+     step += 1
+   end
+   pass += 1
   end
   array
 end
@@ -19,25 +17,17 @@ p bubble_sort([4, 3, 78, 2, 0, 2])
 
 def bubble_sort_by(array)
   n = array.length
-  return array if n <=1 
-
+  return array if n <= 1
   pass = 0
- 
   while pass < n
-
     step = 1
     while step < (n - pass)
-      if yield(array[step - 1], array[step]).positive?
-        array[step - 1], array[step] = array[step], array[step - 1]
-      
-      end
+      array[step - 1], array[step] = array[step], array[step - 1] if yield(array[step - 1], array[step]).positive?
       step += 1
     end
-
-
     pass += 1
   end
   array
 end
 
-p bubble_sort_by(%w(hi hello hey)) { |left, right| left.length - right.length }
+p bubble_sort_by(%w[hi hello hey]) { |left, right| left.length - right.length }
